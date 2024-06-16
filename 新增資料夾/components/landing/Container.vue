0a073b2fced76e6011f1c651b32658e518aee9d9
unknown <fushun891214@gmail.com>
2024-06-16 23:45:49 +0800

    2024/6/16/23:45

--- 新增資料夾/components/landing/Container.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <!-- 定義容器模板 -->
-  <div class="max-w-screen-xl mx-auto px-5">
-    <!-- 插槽，用於插入子組件或內容 -->
-    <slot />
-  </div>
-</template>
--- 新增資料夾/layouts/landing.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <!-- 顯示導航欄 -->
-  <LandingNavbar></LandingNavbar>
-  <!-- 插槽，用於插入外部內容 -->
-  <slot></slot>
-  <!-- 顯示頁腳 -->
-  <LandingFooter></LandingFooter>
-</template>
